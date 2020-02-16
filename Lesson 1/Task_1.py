172a96af80a36e176f8c7f180f7fc247d6461a94
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2020-02-16 21:37:56 +0300

    add files for tasks

---
+++ Lesson 1/Task_1.py
@@ -0,0 +1,5 @@
+#Задача 1
+#Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+# Алгоритм - https://drive.google.com/file/d/1YlfSxx4N27j73qq5emXOdcwZQW38Mwqe/view?usp=sharing
+
+
