332016ec0c5a13d9395217b6c79a5e11be363e83
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2020-02-24 22:05:33 +0300

    solved task_5

--- Lesson 3/task_5.py
+++ Lesson 3/task_5.py
@@ -1,5 +1,26 @@
 # Задача 5
 # В массиве найти максимальный отрицательный элемент.
-# Вывести на экран его значение и позицию в массиве. Примечание к задаче: пожалуйста не путайте
-# «минимальный» и «максимальный отрицательный». Это два абсолютно разных значения.
-# Блок-схема -
+# Вывести на экран его значение и позицию в массиве.
+# Блок-схема - https://drive.google.com/file/d/1BENNIygfWRsItgXe7baFrWQXqRV-tQIA/view?usp=sharing
+
+from random import randint
+
+#my_mssv = [39, -32, -16, 30, -85, 83, 83, -44, 39]
+my_mssv = [randint(-100, 100) for i in range(10)]
+print('Исходный массив:', my_mssv)
+
+position = -1
+min_el = 0
+
+for i in my_mssv:
+
+    if i < 0 and position == -1:
+        min_el = i
+        position = my_mssv.index(i)
+
+    elif min_el < i < 0:
+        min_el = i
+        position = my_mssv.index(i)
+
+print('Позиция максимального отрицательного элемента - ', position + 1)
+print('Максимальный отрицательный элемент - ', min_el)
