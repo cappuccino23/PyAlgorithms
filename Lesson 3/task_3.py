dbbaba3346aa252230db9c3aa883144e5d7ef4b3
alekskiseleva <aleks.kiseleva@gmail.com>
2020-02-27 14:31:06 +0300

    update URL on google disk (block-schema draw.io)

--- Lesson 3/task_3.py
+++ Lesson 3/task_3.py
@@ -1,12 +1,14 @@
 # Задача 3
 # В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
-# Блок-схема -
+# Блок-схема - https://drive.google.com/file/d/1EVjfVBiV0WAgJ0KhTbFDs31skxbVVBoe/view?usp=sharing
 
 from random import randint
 
 my_mssv = [randint(0, 100) for i in range(10)]
 print('Исходный массив:', my_mssv)
 
+mn = 0
+mx = 0
 max_el = my_mssv[0]
 min_el = my_mssv[0]
 
@@ -16,13 +18,14 @@ for i in my_mssv:
         max_el = i
         mx = my_mssv.index(i)
 
-    if i < min_el:
+    elif i < min_el:
         min_el = i
         mn = my_mssv.index(i)
 
 my_mssv[mx], my_mssv[mn] = my_mssv[mn], my_mssv[mx]
 
-print('Массив с переставленными max и min элементами', my_mssv)
 print('Максимальный элемент -', max_el, '\nМинимальный элемент -', min_el)
+print('Массив с переставленными max и min элементами', my_mssv)
+
 
 
