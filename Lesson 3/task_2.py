7d112e2da2999f6a1dc73d0122d265558221c29a
alekskiseleva <aleks.kiseleva@gmail.com>
2020-03-06 13:01:04 +0300

    re-solved task_2

--- Lesson 3/task_2.py
+++ Lesson 3/task_2.py
@@ -6,16 +6,21 @@
 
 from random import randint
 
-my_mssv = [randint(0, 100) for i in range(10)]
+# my_mssv = [randint(0, 100) for i in range(10)]
 # my_mssv = [8, 3, 15, 6, 4, 2] - [0, 3, 4, 5]
 
+my_mssv = [22, 87, 46, 23, 34, 81, 31, 22, 24, 21]
+
+n = 0
 mssv_2 = []
 
 print('Массив:', my_mssv)
 
-for i in my_mssv:
+m = len(my_mssv)
 
-    if i % 2 == 0:
-        mssv_2.append(my_mssv.index(i))
+for i in range(m):
+
+    if my_mssv[i] % 2 == 0:
+        mssv_2.append(i)
 
 print("Индексы четных элементов первого массива:", mssv_2)
