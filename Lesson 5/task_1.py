3a1823549a0603ab1b4845cc243713a6104ab2ec
alekskiseleva <aleks.kiseleva@gmail.com>
2020-03-16 15:42:38 +0300

    частичное решение 1й задачи.

--- Lesson 5/task_1.py
+++ Lesson 5/task_1.py
@@ -3,3 +3,28 @@
 # для каждого предприятия. Программа должна определить среднюю прибыль (за год для всех предприятий)
 # и отдельно вывести наименования предприятий, чья прибыль выше среднего и ниже среднего.
 
+from collections import deque
+
+a = int(input('Введите количество предприятий '))
+i = 1
+profit = 0
+company = {}
+avg_1
+
+for i in range(1, a+1):
+
+    c = input(f'Введите название {i} предприятия: ')
+
+    for j in range(1, 5):
+        b = int(input(f'Введите прибыль за {j} квартал: '))
+        profit = (profit + b)
+
+    company[c] = profit
+
+for v in company:
+
+    avg_1 = company.values() + avg_1
+
+avrg_year = avg_1 / 4
+
+print(company)
