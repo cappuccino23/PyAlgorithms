2196a7dcdc3062625e7a28bc7bd14d7205250f3c
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2020-03-29 14:32:51 +0300

    вариант решения задачи с хэш-функцией

--- Lesson 8/task_1.py
+++ Lesson 8/task_1.py
@@ -4,25 +4,35 @@
 # Примечание: в сумму не включаем пустую строку и строку целиком.
 import hashlib
 
-# mssv = input('Введите строку: ')
-mssv = 'sova'
+
+def sub_str(mssv):
+    bbb = []
+    d = 0
+    mdict = dict()
+    hash_str = hashlib.sha1(mssv.encode('utf-8')).hexdigest()
+    hash_space = hashlib.sha1(''.encode('utf-8')).hexdigest()
+
+    for i in range(len(mssv)):
+        bbb.append(hashlib.sha1(mssv[i].encode('utf-8')).hexdigest())
+        for j in range(i, len(mssv)):
+            s = (hashlib.sha1((mssv[i - 1:j + 1]).encode('utf-8')).hexdigest())
+            if (s != hash_space) and (s != hash_str):
+                bbb.append(hashlib.sha1(s.encode('utf-8')).hexdigest())
+
+
+    for x in bbb:
+        if x not in mdict:
+            mdict[x] = 1
+        else:
+            mdict[x] = mdict[x] + 1
+
+    for k, v in mdict.items():
+        d = d + 1
+
+    return print(f'Количество подстрок - {d-1}')
+
+
+mssv = input('Введите строку: ')
+# mssv = 'bosa'
 print(f'Вывод строки: {mssv}')
-d = 0
-bbb = []
-hash_str = hashlib.sha1(mssv.encode('utf-8')).hexdigest()
-hash_space = hashlib.sha1(''.encode('utf-8')).hexdigest()
-print(hash_str)
-
-for i in range(len(mssv)):
-    bbb.append(hashlib.sha1(mssv[i].encode('utf-8')).hexdigest())
-    for j in range(i, len(mssv)):
-        s = (hashlib.sha1((mssv[i - 1:j + 1]).encode('utf-8')).hexdigest())
-        if (s != hash_space) and (s != hash_str):
-            bbb.append(hashlib.sha1(s.encode('utf-8')).hexdigest())
-
-for el in bbb:
-    d=d+1
-    print(el, end='\n')
-
-
-print(d)
+sub_str(mssv)
