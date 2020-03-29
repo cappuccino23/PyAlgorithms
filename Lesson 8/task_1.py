55add62aee67a5d3e59c1ddd0291213231fc1694
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2020-03-29 12:35:51 +0300

    added solution in task_1 in lesson 8

--- Lesson 8/task_1.py
+++ Lesson 8/task_1.py
@@ -2,3 +2,27 @@
 #
 # Задача: на вход функции дана строка, требуется вернуть количество различных подстрок в этой строке.
 # Примечание: в сумму не включаем пустую строку и строку целиком.
+import hashlib
+
+# mssv = input('Введите строку: ')
+mssv = 'sova'
+print(f'Вывод строки: {mssv}')
+d = 0
+bbb = []
+hash_str = hashlib.sha1(mssv.encode('utf-8')).hexdigest()
+hash_space = hashlib.sha1(''.encode('utf-8')).hexdigest()
+print(hash_str)
+
+for i in range(len(mssv)):
+    bbb.append(hashlib.sha1(mssv[i].encode('utf-8')).hexdigest())
+    for j in range(i, len(mssv)):
+        s = (hashlib.sha1((mssv[i - 1:j + 1]).encode('utf-8')).hexdigest())
+        if (s != hash_space) and (s != hash_str):
+            bbb.append(hashlib.sha1(s.encode('utf-8')).hexdigest())
+
+for el in bbb:
+    d=d+1
+    print(el, end='\n')
+
+
+print(d)
